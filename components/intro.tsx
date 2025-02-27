c66f25614e99bf727a028b5363af4c8a11d63e84
ivan chernov <96547961+codeWeb777@users.noreply.github.com>
2025-02-27 13:00:59 +0300

    add project

---
+++ components/intro.tsx
@@ -0,0 +1,112 @@
+
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import {motion} from "framer-motion";
+import Link from "next/link";
+import {BsArrowRight, BsLinkedin} from "react-icons/bs";
+import {FaGithubSquare, FaTelegram} from "react-icons/fa";
+import {useSectionInView} from "@/lib/hooks";
+import {useActiveSectionContext} from "@/context/active-section-context";
+
+export default function Intro() {
+	const {ref} = useSectionInView("Главная", 0.5);
+	const {setActiveSection, setTimeOfLastClick} = useActiveSectionContext();
+
+	return (
+		<section
+			ref={ref}
+			id="Главная"
+			className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+		>
+			<div className="flex items-center justify-center">
+				<div className="relative">
+					<motion.div
+						initial={{opacity: 0, scale: 0}}
+						animate={{opacity: 1, scale: 1}}
+						transition={{
+							type: "tween",
+							duration: 0.2,
+						}}
+					>
+						<Image
+							src="/my-photo.jpg"
+							alt="my photo"
+							width="190"
+							height="190"
+							quality="100"
+							priority={true}
+							className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-violet-300 shadow-xl"
+						/>
+					</motion.div>
+
+					<motion.span
+						className="absolute bottom-0 right-0 text-4xl"
+						initial={{opacity: 0, scale: 0}}
+						animate={{opacity: 1, scale: 1}}
+						transition={{
+							type: "spring",
+							stiffness: 125,
+							delay: 0.1,
+							duration: 0.7,
+						}}
+					>
+						👋
+					</motion.span>
+				</div>
+			</div>
+
+			<motion.h1
+				className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+				initial={{opacity: 0, y: 100}}
+				animate={{opacity: 1, y: 0}}
+			>
+			<span className='font-bold block'>Привет,меня зовут Иван. <br/> Я frontend / fullstack разработчик.</span>
+			</motion.h1>
+
+			<motion.div
+				className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+				initial={{opacity: 0, y: 100}}
+				animate={{opacity: 1, y: 0}}
+				transition={{
+					delay: 0.1,
+				}}
+			>
+				<Link
+					href="#Контакты"
+					className="group bg-gray-900 text-white px-7 py-3 mr-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+					onClick={() => {
+						setActiveSection("Контакты");
+						setTimeOfLastClick(Date.now());
+					}}
+				>
+					Связаться {" "}
+					<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+				</Link>
+				<a
+					className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://linkedin.com"
+					target="_blank"
+				>
+					<BsLinkedin/>
+				</a>
+
+				<a
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://github.com"
+					target="_blank"
+				>
+					<FaGithubSquare/>
+				</a>
+				<a
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+					href="https://tg.com"
+					target="_blank"
+				>
+					<FaTelegram/>
+				</a>
+			</motion.div>
+		</section>
+	);
+}
